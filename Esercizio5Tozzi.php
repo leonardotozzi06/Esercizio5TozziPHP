5e79acb6ac07a86809aa60ea2c2d252ca2c08e39
leonardotozzi06 <tozzileonardo18@gmail.com>
2024-09-27 07:34:42 +0200

    Esercizio5TozziPHP

---
+++ Esercizio5Tozzi.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $num = rand(1,100);
+
+        if($num % 2 == 0){
+
+            echo "<p>Il numero $num è pari</p>"; 
+
+        } else {
+
+            echo "<p>Il numero $num è dispari</p>";
+
+        }
+
+    ?>
+</body>
+</html>
